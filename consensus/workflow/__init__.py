b72f003df5d98518e1ebc9dffa960d9e577e4a58
Ben Chen <ben.chen.uk@gmail.com>
2026-02-13 23:17:45 +0000

    Removed old basic implementation

--- consensus/workflow/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Workflow Module
-LangGraph-based workflow orchestration
-"""
-
-from consensus.workflow.graph import ResearchGraph
-from consensus.workflow.state import ResearchState
-
-__all__ = ["ResearchGraph", "ResearchState"]
